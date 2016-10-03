1c5c0280b75883b185a49431a0d6ab57812c2fdd
Haley Zeng <haley@ubuntu.ubuntu-domain>
2016-10-02 22:05:01 -0400

    acc registration, login

--- app.py
+++ app.py
@@ -1,52 +1,85 @@
 from flask import Flask, render_template, request
 import hashlib
 
-#this is a constructor call
-#creating an instance of a class
 app = Flask(__name__) 
-print app
-#tells apache what to do when browser requests access from root of flask app
-@app.route("/")
-def helloWorld():
-    print "\n\n\n\n"
-    print "::DIAG:: this flsk obj"
-    print app
-    print "::DIAG:: this request obj"
-    print request
-    print "::DIAG:: request.headers"
-    print request.headers
-    print "::DIAG:: request.method"
-    print request.method
-    print "::DIAG:: request.args"
-    print request.args 
-    print "::DIAG:: request.form"
-    print request.form
-    return render_template("form.html")
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title = "Log in", text = "Log in, if you please.")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html", title = "Account Signup", text = "Create an account")
 
 @app.route("/auth", methods=["POST"])
 def authenticate():
-    username = "arthur"
-    password = "read"
-    
-    passHash = hashlib.sha1()
-    passHash.update(password)
-    hashedPassword = passHash.hexdigest()
-    
-    inputtedUser = request.form['username']
-    inputtedPass = request.form['password']
-
-    inputHash = hashlib.sha1()
-    inputHash.update(inputtedPass)
-    hashedInput = inputHash.hexdigest()
-
-    if username == inputtedUser and hashedPassword == hashedInput:
-        my_title = "Success!"
-        my_text = "Logged in!!! Yessss"
+    dictOfUsers = getDictOfUsers()
+    user = request.form['username']
+    passwd = request.form['password']
+    if not user in dictOfUsers.keys():
+        my_title = "Please register"
+        my_text = "Username not recognized. Please go back and try again, or register a new account."
     else:
-        my_title = "Failure!"
-        my_text = "Authentification failed...... c'mon man....."
+        if hashPW(passwd) == dictOfUsers[user]:
+            my_title = "Success!"
+            my_text = "Logged in!!! Yessss"
+        else:
+            my_title = "Failure!"
+            my_text = "Authentification failed. Incorrect password. Please try again."
+            
     return render_template("output.html", title = my_title, text = my_text)
+
+@app.route("/register", methods=["POST"])
+def register():
+    dictOfUsers = getDictOfUsers()
+    user = request.form['username']
+    pass1 = request.form['password1']
+    pass2 = request.form['password2']
+    if (pass1 != pass2):
+        my_title = "Registration Failure"
+        my_text = "Passwords do not match. Please try again."
+        my_template = "signup.html"
+    else:
+        if user in dictOfUsers.keys():
+            my_title = "Registration Failure"
+            my_text = "Sorry, username is taken! Please enter a different username."
+            my_template = "signup.html"
+        else:
+            dictOfUsers[user] = hashPW(pass1)
+            outputToUserCSV(dictOfUsers)
+            my_title = "Registration Success!"
+            my_text = "Account created! Please log in"
+            my_template = "login.html"
+    return render_template(my_template, title = my_title, text = my_text)
+
+def getDictOfUsers():
+    d = {}
+    userFile = open("data/users.csv", "r")
+    userList = userFile.readlines()
+    for user in userList:
+        userInfo = user.strip().split(",")
+        username = userInfo[0]
+        password = userInfo[1]
+        d[username] = password
+    return d
+
+def outputToUserCSV(userDict):
+    output = ""
+    for user in userDict:
+        output += user + "," + userDict[user] + "\n"
+    output.strip()
+    userFile = open("data/users.csv", "w")
+    userFile.write(output)
+    userFile.close()
+
+def hashPW(pw):
+    hashObj = hashlib.sha1()
+    hashObj.update(pw)
+    return hashObj.hexdigest()
 
 if __name__ == "__main__":
     app.debug = True
